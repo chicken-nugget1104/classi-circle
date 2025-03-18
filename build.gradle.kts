b81e971d3fa6e443feb5ef367d72b4775ec682be
jmxamongusmodder <160080970+chicken-nugget1104@users.noreply.github.com>
2025-03-18 15:53:34 -0400

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ version = "0.0.3A-pubtestfacesneww"
 repositories {
     mavenCentral()
     jcenter()
+    maven { url = uri("https://m2.dv8tion.net/releases") }
 }
 
 val natives: Configuration by configurations.creating
@@ -20,7 +21,10 @@ dependencies {
     natives(group = "org.lwjgl.lwjgl", name = "lwjgl-platform", version = "2.9.3", classifier = "natives-windows")
     natives(group = "org.lwjgl.lwjgl", name = "lwjgl-platform", version = "2.9.3", classifier = "natives-linux")
     natives(group = "org.lwjgl.lwjgl", name = "lwjgl-platform", version = "2.9.3", classifier = "natives-osx")
-    implementation("com.jagrosh:DiscordIPC:0.4")
+    implementation(files("libs/DiscordIPC-0.4.jar"))
+    implementation("org.json:json:20210307")
+    implementation("org.slf4j:slf4j-api:1.7.36") // SLF4J API
+    implementation("org.slf4j:slf4j-simple:1.7.36") // Basic implementation
 }
 
 
@@ -36,8 +40,17 @@ task("extractNatives", Copy::class) {
     dependsOn(natives)
     from(natives.map { zipTree(it) })
     into("${project.projectDir.toPath()}\\run\\natives")
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
 
 application {
     mainClassName = "com.mojang.minecraft.Minecraft"
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+            "Main-Class" to "com.mojang.minecraft.Minecraft"
+        )
+    }
 }
--- src/main/java/com/mojang/minecraft/Minecraft.java
+++ src/main/java/com/mojang/minecraft/Minecraft.java
@@ -45,10 +45,6 @@ import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.util.glu.GLU.gluPerspective;
 import static org.lwjgl.util.glu.GLU.gluPickMatrix;
 
-import com.jagrosh.discordipc.IPCClient;
-import com.jagrosh.discordipc.IPCListener;
-import com.jagrosh.discordipc.entities.RichPresence;
-
 public class Minecraft implements Runnable {
 
     private final Timer timer = new Timer(20);
@@ -69,9 +65,6 @@ public class Minecraft implements Runnable {
 
     private boolean shouldTakeScreenshot = false;
     private Tile[] tileSelectionOrder = {Tile.grass, Tile.rock, Tile.dirt, Tile.stoneBrick, Tile.wood, Tile.bush, Tile.rose, Tile.sand, Tile.brick};
-
-    private static final long CLIENT_ID = 1255593789746319360L;
-    private static IPCClient client;
 
     public int SIZE_X = 256;
     public int SIZE_Z = 64;
@@ -948,35 +941,11 @@ public class Minecraft implements Runnable {
     }
 
     private static void initDiscordRPC() {
-        client = new IPCClient(CLIENT_ID);
-
-        client.setListener(new IPCListener() {
-            @Override
-            public void onReady(IPCClient client) {
-                System.out.println("Connected to Discord!");
-                updateDiscordPresence("Classicircle", "Playing a world");
-            }
-        });
-
-        try {
-            client.connect();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            client.close();
-        }));
+	return;
     }
 
     private static void updateDiscordPresence(String state, String details) {
-        RichPresence.Builder builder = new RichPresence.Builder();
-        builder.setState(state)
-               .setDetails(details)
-               .setStartTimestamp(OffsetDateTime.now())  // Use OffsetDateTime.now()
-               .setLargeImage("large_image_key", "Large Image Text")
-               .setSmallImage("small_image_key", "Small Image Text");
-
-        client.sendRichPresence(builder.build());
+       	//null
+	return;
     }
 }
